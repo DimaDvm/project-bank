9d19108ddb0a952488f15f00d4c09bc3976c2cac
Dima <dmitriy.movchan.02@gmail.com>
2023-07-24 13:26:25 +0300

    test

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { App } from './App';
 
 const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
